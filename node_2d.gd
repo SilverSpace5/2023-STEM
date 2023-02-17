558e314c8858dd3386e25295a8a492322e291cc4
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2023-02-17 18:08:06 +1030

    hills

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
 func _ready():
-	for x in range(200):
-		$TileMap.set_cell(0, Vector2i(x, 0), 0, Vector2i(0, 0))
+	var height = randi_range(20, 80)
+	for x in range(100):
+		height += randi_range(-1, 1)
+		height = clampi(height, 20, 80)
+		for y in range(height):
+			$TileMap.set_cell(0, Vector2i(x, -y), 0, Vector2i(0, 1))
+		$TileMap.set_cell(0, Vector2i(x, -height), 0, Vector2i(0, 0))
